82ee679c3d67f112039e25c2a22e3e49e74f4efe
Md.Sawrab <mdsawrab02@gmail.com>
2024-02-10 00:00:52 +0600

    Update Type of Triangle.sql

--- Advanced Select/Type of Triangle.sql
+++ Advanced Select/Type of Triangle.sql
@@ -8,3 +8,18 @@ WHEN A = B OR B = C OR A = C THEN 'Isosceles'
 ELSE 'Scalene'
 END
 FROM TRIANGLES;
+
+ -- OR
+
+SELECT
+  CASE
+    WHEN A = B AND B = C then "Equilateral"
+    WHEN A + B <= C then "Not A Triangle"
+    WHEN A + C <= B then "Not A Triangle"
+    WHEN B + C <= A then "Not A Triangle"
+    WHEN A = B AND A <> C then "Isosceles"
+    WHEN A = C AND A <> B then "Isosceles"
+    WHEN B = C AND A <> B then "Isosceles"
+    ELSE "Scalene"
+  END
+FROM Triangles;
