d2c5956900669e7beda6af4cec2885579b3f206e
Md.Sawrab <mdsawrab02@gmail.com>
2024-02-09 23:50:50 +0600

    Create Weather Observation Station 12.sql

---
+++ Basic Select/Weather Observation Station 12.sql
@@ -0,0 +1,14 @@
+-- Weather Observation Station 12
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+-- https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+
+SELECT DISTINCT City
+FROM Station
+WHERE LEFT(City, 1) NOT IN ('A', 'E', 'I', 'O', 'U') AND
+      RIGHT(City, 1) NOT IN ('A', 'E', 'I', 'O', 'U');
+
+ -- OR 
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[^aeiouAEIOU]' AND CITY REGEXP '[^aeiouAEIOU]$';
