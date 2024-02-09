80e1a120f17bb55cda63e300f5c460a5c5913cb3
Md.Sawrab <mdsawrab02@gmail.com>
2024-02-09 21:45:01 +0600

    Create Weather Observation Station 7.sql

---
+++ Basic Select/Weather Observation Station 7.sql
@@ -0,0 +1,23 @@
+-- Weather Observation Station 7
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+-- https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+
+SELECT DISTINCT City
+FROM Station
+WHERE RIGHT(City, 1) IN ('a', 'e', 'i', 'o', 'u');
+
+OR
+  
+SELECT DISTINCT City
+FROM Station
+WHERE city LIKE '%a' 
+   OR city LIKE '%e' 
+   OR city LIKE '%i' 
+   OR city LIKE '%o' 
+   OR city LIKE '%u';
+
+OR
+
+SELECT DISTINCT City
+FROM Station
+WHERE city REGEXP '[aeiou]$';  -- RLIKE is the synonym.
